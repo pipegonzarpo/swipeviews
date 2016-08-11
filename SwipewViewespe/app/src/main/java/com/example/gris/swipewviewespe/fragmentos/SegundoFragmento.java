c3550010493f17cbed105504fccda1bdacf3181d
pipegonzarpo <threediseno@gmail.com>
2016-08-10 19:02:42 -0500

    cargAR
    swipeviews

---
+++ SwipewViewespe/app/src/main/java/com/example/gris/swipewviewespe/fragmentos/SegundoFragmento.java
@@ -0,0 +1,31 @@
+package com.example.gris.swipewviewespe.fragmentos;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gris.swipewviewespe.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SegundoFragmento extends Fragment {
+
+
+    public SegundoFragmento() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_segundo_fragmento, container, false);
+    }
+
+}
